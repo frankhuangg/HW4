6a160505fae1c930a4949c91496229d431ca3689
frankhuangg <79563470+frankhuangg@users.noreply.github.com>
2021-05-11 17:03:17 +0800

    HW5

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import render from './HW5.js';
+import './App.css';
+
+function App() {
+ return (
+    <div className="App">
+        <div>
+            { render() }
+        </div>
+    </div>
+ );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import HW5 from './HW5.js';
+import UserGithub from './HW5';
+
+
+// ReactDOM.render(
+//   <UserGithub source="https://api.github.com/users/frankhuangg" />,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
